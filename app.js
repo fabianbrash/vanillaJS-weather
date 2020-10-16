cf3d39ae743fc170e5aeb39beb691c483bd210b7
Fabian Brash <fabianbrash@gmail.com>
2020-10-16 14:26:52 -0400

    added fahrenheit conversion

--- app.js
+++ app.js
@@ -78,3 +78,29 @@ const displayWeather = () => {
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
+
+
+// CONVERT CELSIUS TO FAHRENHEIT
+
+const celsiusToFahrenheit = (temperature) => {
+    return (temperature *9/5) + 32;
+}
+
+// LET'S GET USER CLICK
+
+tempElement.addEventListener("click", () => {
+
+    if(weather.temperature.value === undefined) return;
+
+     if(weather.temperature.unit == "celsius") {
+         let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+         fahrenheit = Math.floor(fahrenheit);
+
+         tempElement.innerHTML = `${fahrenheit}&#176;<span>F</span>`;
+         weather.temperature.unit = "fahrenheit";
+     }
+     else {
+         tempElement.innerHTML = `${weather.temperature.value}&#176;<span>C</span>`;
+         weather.temperature.unit = "celsius";
+     }
+})
